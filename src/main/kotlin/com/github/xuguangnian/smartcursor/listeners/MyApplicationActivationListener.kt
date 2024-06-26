333c67057737aac28beae358e034ecb06bb1641e
GitHub Action <action@github.com>
2024-06-26 02:03:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/xuguangnian/smartcursor/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/xuguangnian/smartcursor/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.xuguangnian.smartcursor.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/xuguangnian/smartcursor/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/xuguangnian/smartcursor/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.xuguangnian.smartcursor.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.xuguangnian.smartcursor.MyBundle
+import com.github.xuguangnian.smartcursor.services.MyProjectService
 import javax.swing.JButton
 
 
